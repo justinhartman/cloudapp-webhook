d938aeb971aa35159fe1a422416d71a5e02ba05a
Justin Hartman <justin@hartman.me>
2020-03-31 01:30:25 +0200

    Rebuilt index to save items to DB only. Updated bootstrap.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * CloudApp Webhook.
+ * CloudApp Webhook - Database only.
  *
- * This file implements the webhook app for CloudApp.
+ * This file implements the webhook app for CloudApp which inserts a record into
+ * the SQLite database only.
  *
  * PHP version 7.3
  *
@@ -12,13 +13,13 @@
  * @copyright 2020 Cape Town Creative Academy (Pty) Limited
  * @license   https://gitlab.com/ctca/cloudapp-webhook/-/blob/master/LICENSE (c)
  * @link      https://gitlab.com/ctca/cloudapp-webhook
- * @since     0.2.0
+ * @since     1.3.0
  */
 require_once __DIR__.'/bootstrap.php';
 
-use App\Upload;
-use App\Download;
 use App\Log;
+use App\Database\SqliteConnect;
+use App\Database\SqliteInsert;
 
 /**
  * Check the request type
@@ -51,6 +52,28 @@ function payload()
     $data = json_decode($json, true);
 
     return $data;
+}
+
+/**
+ * DB Insert
+ *
+ * @param string $event    The payload event.
+ * @param string $itemName The payload item name.
+ * @param string $itemUrl  The payload item url.
+ * @param string $created  The payload created date.
+ *
+ * @return void
+ */
+function dbInsert($event, $itemName, $itemUrl, $created)
+{
+    // Setup the DB PDO.
+    $pdo = (new SqliteConnect())->connect();
+    $sqlite = new SqliteInsert($pdo);
+
+    // Build the query
+    $insert = $sqlite->insert($event, $itemName, $itemUrl, $created);
+
+    return $insert;
 }
 
 // Setup the log file.
@@ -93,30 +116,22 @@ $payload = payload();
 $payloadEvent = $payload['event'];
 $payloadName  = $payload['payload']['item_name'];
 $payloadUrl   = $payload['payload']['item_url'];
-
-// Load classes.
-$download = new Download;
-$upload   = new Upload;
-
-// Get download URL from the Payload URL.
-$fileUrl = $download->downloadUrl($payloadUrl);
-
-// Save media to file path.
-$savePath = $download->saveTo($payloadName);
-
+$payloadDate  = $payload['payload']['created_at'];
 
 try {
-    //Open file handler.
-    $file = fopen($savePath, 'w+');
-
-    //If $file is FALSE, something went wrong.
-    if ($file === false) {
-        throw new Exception('Could not open: ' . $savePath);
-    }
+    dbInsert($payloadEvent, $payloadName, $payloadUrl, $payloadDate);
+    $logger->info(
+        sprintf(
+            'Inserted %s (%s) created at %s into SQLite Database.',
+            $payloadName,
+            $payloadUrl,
+            $payloadDate
+        )
+    );
 } catch (Exception $e) {
     $logger->error(
         sprintf(
-            'Index savePath error. Code: %s. Error: %s.',
+            'Index dbInsert error code %s. Error: %s',
             $e->getCode(),
             $e->getMessage()
         )
@@ -128,91 +143,6 @@ try {
         'payload' =>
             array(
                 'error' => $e->getMessage()
-            )
-        )
-    );
-
-    exit;
-}
-
-try {
-    //Create a cURL handle.
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $fileUrl);
-
-    // Options
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_AUTOREFERER, false);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_REFERER, "https://media.ctca.co.za");
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    //Pass our file handle to cURL.
-    // curl_setopt($ch, CURLOPT_FILE, $file);
-
-    //Timeout if the file doesn't download after 20 seconds.
-    // curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-
-    //Execute the request.
-    // curl_exec($ch);
-    $result = curl_exec($ch);
-
-    //If there was an error, throw an Exception
-    if (curl_errno($ch)) {
-        throw new Exception(curl_error($ch));
-    }
-} catch (Exception $e) {
-    $logger->error(
-        sprintf(
-            'Index cURL error. Code: %s. Error: %s.',
-            $e->getCode(),
-            $e->getMessage()
-        )
-    );
-    echo json_encode(
-        array(
-        'event' => 'error',
-        'code' => $e->getCode(),
-        'payload' =>
-            array(
-                'error' => $e->getMessage()
-            )
-        )
-    );
-
-    exit;
-}
-
-//Get the HTTP status code.
-$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-fwrite($file, $result);
-
-//Close the cURL handler.
-curl_close($ch);
-
-//Close the file handler.
-fclose($file);
-
-if ($statusCode == 200) {
-    return $upload->uploadVideo($savePath, $payloadName);
-} else {
-    $logger->error(
-        sprintf(
-            'Index statusCode error. Code: %s. There was an error with the download request.',
-            $statusCode
-        )
-    );
-    // JSON payload.
-    echo json_encode(
-        array(
-        'event' => 'error',
-        'code' => $statusCode,
-        'payload' =>
-            array(
-                'There was an error with the download request.'
             )
         )
     );
