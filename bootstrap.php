eca98ab1cb8694a4a920dca02e9722a304222e3a
Justin Hartman <justin@hartman.me>
2020-03-30 03:19:24 +0200

    Added Bootstrap configuration file to load classes and set values.

---
+++ bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Bootstrap
+ *
+ * PHP version 7.3
+ *
+ * @category  App
+ * @package   Base
+ * @author    Justin Hartman <j.hartman@ctca.co.za>
+ * @copyright 2020 Cape Town Creative Academy (Pty) Limited
+ * @license   https://gitlab.com/ctca/cloudapp-webhook/-/blob/master/LICENSE (c)
+ * @link      https://gitlab.com/ctca/cloudapp-webhook
+ * @since     0.0.1
+ */
+require_once __DIR__.'/vendor/autoload.php';
+
+// Set unlimited memory (bad idea).
+// ini_set('memory_limit', '-1');
+ini_set('memory_limit', '1024M');
+
+// Set the application header to JSON.
+header('Content-Type: application/json');
+
+/**
+ * Autoload Class
+ *
+ * @return object Class
+ */
+function autoload()
+{
+    include_once getcwd()."/classes/Download.php";
+    include_once getcwd()."/classes/Log.php";
+    include_once getcwd()."/classes/Upload.php";
+}
+
+// Autoload the Class files.
+spl_autoload_register("autoload");
