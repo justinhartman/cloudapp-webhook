d23cfce117fbf87e4565fb736ba672f218e603b9
Justin Hartman <justin@hartman.me>
2020-03-30 03:26:08 +0200

    Minor updates to version numbers.

--- classes/Log.php
+++ classes/Log.php
@@ -1,11 +1,26 @@
 <?php
+/**
+ * Log file.
+ *
+ * Methods to log items to the log file.
+ *
+ * PHP version 7.3
+ *
+ * @category  App
+ * @package   Log
+ * @author    Justin Hartman <j.hartman@ctca.co.za>
+ * @copyright 2020 Cape Town Creative Academy (Pty) Limited
+ * @license   https://gitlab.com/ctca/cloudapp-webhook/-/blob/master/LICENSE (c)
+ * @link      https://gitlab.com/ctca/cloudapp-webhook
+ * @since     0.1.0
+ */
 namespace App;
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 /**
- * File Download Class
+ * Log Class
  *
  * {inheritdoc}
  */
