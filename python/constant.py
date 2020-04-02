ede213628f670928140b6d4f852d36047c6d1553
Justin Hartman <justin@hartman.me>
2020-04-03 01:06:54 +0200

    New constants and mime-types python files.

---
+++ python/constant.py
@@ -0,0 +1,10 @@
+"""
+Declare constants for use in the application.
+"""
+# MEDIA_PATH = '../media/'
+MEDIA_PATH = '/srv/data/web/vhosts/cloudapp.hartman.me/htdocs/media/'
+MEDIA_IMAGE = MEDIA_PATH + 'images/'
+MEDIA_OTHER = MEDIA_PATH + 'other/'
+MEDIA_VIDEO = MEDIA_PATH + 'videos/'
+HTTP_URL = 'https://media.ctca.co.za'
+DATABASE_PATH = "../database/database.sqlite"
