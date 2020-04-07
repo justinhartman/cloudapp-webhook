54e74cfbf40917738a2edc71b56f785602f484a3
Justin Hartman <justin@hartman.me>
2020-04-07 11:30:59 +0200

    Fixed upload.py to work on server.

--- modules/upload.py
+++ modules/upload.py
@@ -3,52 +3,20 @@ import sys
 sys.path.append('../')
 import app
 sys.path.append(app.GOOGLE_PACKAGES)
-import json
-# import media
-from apiclient import discovery, errors
-from httplib2 import Http
-# from oauth2client import client, file, tools
+from apiclient import discovery
 from apiclient.http import MediaFileUpload
+
 
 def get_client():
     from google.oauth2 import service_account
-    import googleapiclient.discovery
 
     SCOPES = ['https://www.googleapis.com/auth/drive']
     SERVICE_ACCOUNT_FILE = './credentials.json'
-    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
 
     return credentials
-
-# define a function to retrieve all files
-def retrieve_all_files(api_service):
-    results = []
-    page_token = None
-
-    while True:
-        try:
-            param = {}
-
-            if page_token:
-                param['pageToken'] = page_token
-
-            files = api_service.files().list(**param).execute()
-            # append the files from the current result page to our list
-            results.extend(files.get('files'))
-            # Google Drive API shows our files in multiple pages when the number of files exceed 100
-            page_token = files.get('nextPageToken')
-
-            if not page_token:
-                break
-
-        except errors.HttpError as error:
-            print(f'An error has occurred: {error}')
-            break
-    # output the file metadata to console
-    for file in results:
-        print(file)
-
-    return results
 
 
 def upload_media(credentials, filename):
@@ -66,9 +34,9 @@ def upload_media(credentials, filename):
     media = MediaFileUpload(media.save_path(filename),
                             mimetype='application/octet-stream')
     file = connection.files().create(body=file_metadata,
-                                        media_body=media,
-                                        fields='id,name',
-                                        supportsAllDrives=True).execute()
+                                     media_body=media,
+                                     fields='id,name',
+                                     supportsAllDrives=True).execute()
 
     file_id = file.get('id')
     file_name = file.get('name')
