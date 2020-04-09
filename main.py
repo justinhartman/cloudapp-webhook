4ba0ff11643a053d35c45939d886c06403335534
Justin Hartman <justin@hartman.me>
2020-04-09 11:19:42 +0200

    - New timestamp methods in utility.py for log file - Fixed comment blocks in utility.py. - Updated main.py.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+The main application script.
+
+This script contains all the application code for downloading media from
+CloudApp and then uploading them to Google Drive using the Google Drive API.
+"""
+import time
 import app
 import db
 import downloads
@@ -8,16 +16,16 @@ import utility
 
 
 def main():
-    print('-------------------------------------------------------------------\
--------------')
-    print(utility.date_time(2) + " Starting the downloads.")
+    """Return the main application method."""
+    started = time.time()
+    utility.timestamp_top()
     # Path the database.
     database = app.DATABASE_PATH
     # create a database connection.
-    print(utility.date_time(2) + " Opening the database connection.")
+    utility.timestamp_message("Opening the database connection.")
     conn = db.create_connection(database)
     with conn:
-        print(utility.date_time(2) + " Getting list of records from database.")
+        utility.timestamp_message("Getting list of records from database.")
         rows = db.select_all(conn)
         for row in rows:
             """
@@ -34,7 +42,7 @@ def main():
             Make sure the media folder actually exists.
             """
             if utility.check_path(app.MEDIA_PATH) is False:
-                print(utility.date_time(2) + " ERROR: Media folder not valid.")
+                utility.timestamp_message("ERROR: Media folder not valid.")
                 return 1
 
             """
@@ -54,38 +62,41 @@ def main():
             """
             Download the file to server.
             """
-            print(utility.date_time(2) + " Downloading \"" + name + "\"")
+            utility.timestamp_message("Downloading \"" + name + "\"")
             status, size = downloads.download_file(name, download_url)
-            print(utility.date_time(2)+" "+str(status)+": "+name+" ("+link+")")
+            utility.timestamp_message(str(status)+": "+name+" ("+link+")")
 
             """
             Update database if successfull.
             """
             if status == 200:
-                insert_id = db.insert_record(conn, item_id, size, status)
-                print(utility.date_time(2)+" "+str(insert_id)+" inserted in downloads.")
+                rid = db.insert_record(conn, item_id, size, status)
+                utility.timestamp_message(str(rid)+" inserted in downloads.")
                 db.update_record(conn, 1, item_id)
-                print(utility.date_time(2)+" Updated id "+str(item_id)+".")
+                utility.timestamp_message("Updated ID " + str(item_id))
 
                 # Make connection to Google Drive API.
                 credentials = upload.get_client()
                 # Upload file to API.
-                print(utility.date_time(2)+" Uploading to Google Drive.")
+                utility.timestamp_message("Uploading to Google Drive.")
                 file_id, file_name = upload.upload_media(credentials, name)
                 # Insert Upload into DB.
-                db.insert_upload(conn, insert_id, file_id, file_name)
-                print(utility.date_time(2)+" "+file_id+" inserted in uploads.")
+                db.insert_upload(conn, rid, file_id, file_name)
+                utility.timestamp_message(file_id+" inserted in uploads.")
             elif status == 404:
                 db.update_record(conn, 2, item_id)
-                print(utility.date_time(2) + " 404 file not found.")
+                utility.timestamp_message("404 file not found.")
             else:
                 print(utility.date_time(2) + " General error")
+                utility.timestamp_message("General Unknown Error.")
         else:
-            print(utility.date_time(2) + " Closing the database connection.")
-            print('------------------------------------------------------------\
---------------------')
+            utility.timestamp_message("Closing the database connection.")
     conn.close()
+    completed = time.time()
+    utility.timestamp_tail(completed, started)
+
     return bool(1)
+
 
 if __name__ == '__main__':
     main()
--- modules/utility.py
+++ modules/utility.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+Utility module.
+
+A module containing various utility methods used in the application.
+"""
 from datetime import datetime, timedelta
 import math
 import os.path
 import random
 import string
+import time
 
 
 def check_path(path):
@@ -14,7 +21,7 @@ def check_path(path):
     :type  path: string
 
     :returns: True or false.
-    :rtype:   Boolean
+    :rtype:   boolean
     """
     check = os.path.exists(path)
 
@@ -26,31 +33,32 @@ def convert_size(size_bytes):
     Convert bytes into human readable format.
 
     :param size_bytes: The size in bytes.
-    :type  size_bytes: Integer
+    :type  size_bytes: integer
 
     :returns: Human readable format.
-    :rtype:   String
+    :rtype:   string
     """
     if size_bytes == 0:
         return "0B"
 
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
+    size_a = int(math.floor(math.log(size_bytes, 1024)))
+    size_b = math.pow(1024, size_a)
+    size_c = round(size_bytes / size_b, 2)
 
-    return "%s %s" % (s, size_name[i])
+    return "%s %s" % (size_c, size_name[size_a])
 
 
-def date_time(offset):
+def date_time(offset=0):
     """
-    Returns a formatted current date and time.
+    Return a formatted current date and time.
 
     :param offset: An offset to accommodate for any server time differences.
-    :type  offset: Integer
+                   Defaults to zero if no param set.
+    :type  offset: integer
 
-    :returns: Formatted date time.
-    :rtype:   String
+    :returns: Date formatted like 2020-04-08 09:25:07.
+    :rtype:   string
     """
     today = datetime.now() + timedelta(hours=offset)
     formatted = today.strftime('%F %T')
@@ -60,10 +68,11 @@ def date_time(offset):
 
 def random_string(size=32):
     """
-    Generates a random string.
+    Generate a random string.
 
-    :param size: The length of the string to create.
-    :type  size: number
+    :param   size: The length of the string to create.
+    :type    size: number
+    :default size: 32
 
     :returns: Random string
     :rtype:   string
@@ -72,3 +81,112 @@ def random_string(size=32):
     joined = ''.join(random.choice(chars) for x in range(size))
 
     return joined
+
+
+def timestamp_top():
+    """
+    Return a header for the log file.
+
+    :returns: Multiple information lines.
+    :rtype:   string
+    """
+    head = '===================================================================\
+============='
+    subj = "CloudApp/Google Drive Python Script Log"
+    line = '-------------------------------------------------------------------\
+-------------'
+    build = print(head), print(subj), print(line)
+
+    return build
+
+
+def timestamp_tail(end, start):
+    """
+    Return a footer containing the time to run script for the log file.
+
+    :param end:   The time the script ended.
+    :type  end:   object
+    :param start: The time the script started.
+    :type  start: object
+
+    :returns: Multiple information lines.
+    :rtype:   string
+    """
+    value = time_formatter(end, start)
+    head = '===================================================================\
+============='
+    subj = "Script took %d min %d seconds to run." % (value[2], value[3])
+    line = '-------------------------------------------------------------------\
+-------------'
+    build = print(line), print(subj), print(head)
+    # build = head, subj, line
+    # message = print(build)
+
+    return build
+
+
+def timestamp_message(message):
+    """
+    Return a formatted log file entry with date and time.
+
+    :param message: The message to include in the log file.
+    :type  message: object
+
+    :returns: Formatted log file message.
+    :rtype:   string
+    """
+    clean = str(message)
+    build = date_time(2) + " :~$ " + clean
+    message = print(build)
+
+    return message
+
+
+def time_to_seconds(datetime_string):
+    """
+    Convert a datetime string to a float for converting.
+
+    :param datetime_string: The datetime string
+    :type  datetime_string: string
+
+    :returns: Seconds timestamp
+    :rtype:   float
+    """
+    element = datetime.strptime(
+        datetime_string,
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    time_tuple = element.timetuple()
+    timestamp = time.mktime(time_tuple)
+
+    return timestamp
+
+
+def time_formatter(older, newer):
+    """
+    Convert two time floats from seconds into day, hour, minute and seconds.
+
+    :param older: The older time.
+    :type  older: float
+    :param newer: The newer time.
+    :type  newer: float
+
+    :returns: Mixed variables for day, hour, minute and second.
+    :rtype:   float
+    """
+    diff = int(older - newer)
+
+    time_float = float(diff)
+    day = time_float // (24 * 3600)
+
+    time_float = time_float % (24 * 3600)
+    hour = time_float // 3600
+
+    time_float %= 3600
+    minutes = time_float // 60
+
+    time_float %= 60
+    seconds = time_float
+
+    return day, hour, minutes, seconds
