3fa07171a90aedca1c2b032a79b7fe66963eeddb
Justin Hartman <justin@hartman.me>
2020-04-19 03:43:24 +0200

    Updated the python git commit.

--- modules/git.py
+++ modules/git.py
@@ -10,14 +10,15 @@ from utility import Utility
 
 def commit_db():
     utl = Utility()
-    add = ['git', 'add', 'database/database.sqlite']
-    process_add = utl.sub_process(add)
+    # add = ['git', 'add', 'database/database.sqlite']
+    # process_add = utl.sub_process(add)
 
-    message = str('Automated Commit: Updated database.')
+    message = str('Automated: Python DB and Logs.')
     commit = ['git', 'commit', '-am', message]
     process_com = utl.sub_process(commit)
 
     push = ['git', 'push', '-u', 'origin', 'master']
     process_psh = utl.sub_process(push)
 
-    return process_add, process_com, process_psh
+    # return process_add, process_com, process_psh
+    return process_com, process_psh
