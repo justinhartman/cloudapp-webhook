772d94d8e2f2843889a457e8f9a4ed319cdb743a
Justin Hartman <justin@hartman.me>
2020-04-19 02:56:58 +0200

    New php method and shell script to commit git repo

---
+++ bin/webhook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Webhook script to commit files to the repo.
+#
+# Author: Justin Hartman <j.hartman@ctca.co.za>
+# Version: 1.0.0
+# Copyright (c) 2020 Creative Academy <https://creativeacademy.ac.za>
+#
+
+#######################################
+# Commit database changes to repo.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+commit_db () {
+    git add /app/database/database.sqlite
+    git commit -m "Automated: New webhook payload added to DB."
+    git push -u origin master
+
+    return
+}
+
+commit_db
+exit 0
